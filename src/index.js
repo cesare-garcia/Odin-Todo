e356d50400a7fc058c5d4711e1ccc501c6a8caf5
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-06-28 10:16:11 -0500

    add basic logic to load page and respond to event handlers on page tabs. ensure html works properly

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import './style.css';
-console.log("hey");
+import loadPage from './loadPage.js';
+
+const contentDiv = document.querySelector("#content");
+
+loadPage(contentDiv);
+
+const allTasksTab = document.querySelector("#allTasks");
+const todayTab = document.querySelector("#today");
+const weekTab = document.querySelector("#week");
+const projectsTab = document.querySelector("#projects");
+const taskDisplayBox = document.querySelector(".taskDisplay");
+
+allTasksTab.addEventListener("click", (e) => {
+    console.log("allTasks");
+})
+
+todayTab.addEventListener("click", (e) => {
+    console.log("today");
+})
+
+weekTab.addEventListener("click", (e) => {
+    console.log("week");
+})
+
+projectsTab.addEventListener("click", (e) => {
+    console.log("projects");
+})
