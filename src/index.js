9d05138313cead1b62827da5dfbf9652ea2c24d9
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-06-28 14:34:01 -0500

    add test objects in taskArray, create empty project array, add comments indicating functions necessary for each tab

--- src/index.js
+++ src/index.js
@@ -9,19 +9,45 @@ const contentDiv = document.querySelector("#content");
 
 loadPage(contentDiv);
 
+let tasksArray = [
+    {
+        projectGroup: "",
+        status: "done",
+        title: "Test Task",
+        description: "Create a test task for the project",
+        dueDate: "january 1st, 2024",
+        priority: "high",
+        notes: "make sure to complete three days before."
+    },
+    {
+        projectGroup: "",
+        status: "done",
+        title: "Test Task 2",
+        description: "Create a test task for the project",
+        dueDate: "january 21st, 2024",
+        priority: "high",
+        notes: "make sure to complete three days before."
+    }
+];
+let projectArray = [];
 const allTasksTab = document.querySelector("#allTasks");
 const todayTab = document.querySelector("#today");
 const weekTab = document.querySelector("#week");
 const projectsTab = document.querySelector("#projects");
 const taskDisplayBox = document.querySelector(".taskDisplay");
 
-loadAllTasks(taskDisplayBox);
+loadAllTasks(taskDisplayBox, tasksArray);
 
 allTasksTab.addEventListener("click", (e) => {
     if (taskDisplayBox.children.length > 0 ) {
         let removedElement = taskDisplayBox.firstChild;
         taskDisplayBox.removeChild(removedElement);
-        loadAllTasks(taskDisplayBox);
+        
+        // code to add inputs for a new task into an object that is added to an array
+        
+        
+        // code to display the tasks in the array
+        loadAllTasks(taskDisplayBox, tasksArray);
     }
 })
 
@@ -29,6 +55,13 @@ todayTab.addEventListener("click", (e) => {
     if (taskDisplayBox.children.length > 0 ) {
         let removedElement = taskDisplayBox.firstChild;
         taskDisplayBox.removeChild(removedElement);
+        
+        // code to add inputs for a new task into an object that is added to an array
+        
+        
+        // code to display the tasks in the array
+        
+        
         loadTodaysTasks(taskDisplayBox);
     }
 })
@@ -37,6 +70,13 @@ weekTab.addEventListener("click", (e) => {
     if (taskDisplayBox.children.length > 0 ) {
         let removedElement = taskDisplayBox.firstChild;
         taskDisplayBox.removeChild(removedElement);
+        
+        // code to add inputs for a new task into an object that is added to an array
+        
+        
+        // code to display the tasks in the array
+        
+        
         loadWeeksTasks(taskDisplayBox);
     }
 })
@@ -45,6 +85,13 @@ projectsTab.addEventListener("click", (e) => {
     if (taskDisplayBox.children.length > 0 ) {
         let removedElement = taskDisplayBox.firstChild;
         taskDisplayBox.removeChild(removedElement);
+        
+        // code to add inputs for a new task into an object that is added to an array
+        
+        
+        // code to display the tasks in the array
+        
+        
         loadProjects(taskDisplayBox);
     }
 })
