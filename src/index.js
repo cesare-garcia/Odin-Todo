58466b656c26c1f5e4f44d26fe7cd32b088948c5
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-07-01 15:38:16 -0500

    allow users to select and add tasks to the home tab

--- src/index.js
+++ src/index.js
@@ -1,101 +1,136 @@
 import './style.css';
 import loadPage from './loadPage.js';
-import loadAllTasks from './loadAllTasks.js';
-import loadTodaysTasks from './loadTodaysTasks.js';
-import loadWeeksTasks from './loadThisWeeksTasks.js';
-import loadProjects from './loadProjects.js';
+import buildTask from './buildTask.js';
+import showHomeHeader from './showHomeHeader.js';
+import displayTasks from './displayTasks.js';
 
+let tasks = [];
+let projects = [];
 const contentDiv = document.querySelector("#content");
 
 loadPage(contentDiv);
-
-let tasksArray = [
-    {
-        projectGroup: "",
-        status: "done",
-        title: "Test Task",
-        description: "Create a test task for the project",
-        dueDate: "january 1st, 2024",
-        priority: "high",
-        notes: "make sure to complete three days before."
-    },
-    {
-        projectGroup: "",
-        status: "done",
-        title: "Test Task 2",
-        description: "Create a test task for the project",
-        dueDate: "january 21st, 2024",
-        priority: "high",
-        notes: "make sure to complete three days before."
-    },
-];
-let projectArray = [];
-const allTasksTab = document.querySelector("#allTasks");
+const display = document.querySelector(".display");
+const homeTab = document.querySelector("#home");
 const todayTab = document.querySelector("#today");
 const weekTab = document.querySelector("#week");
 const projectsTab = document.querySelector("#projects");
-const taskDisplayBox = document.querySelector(".taskDisplay");
 
-loadAllTasks(taskDisplayBox, tasksArray);
-// while need an event listener here.
+showHomeHeader();
+const addTaskButton = document.querySelector(".addTask");
+const submit = document.querySelector(".submitTask");
+const form = document.querySelector("#ntForm");
 
-allTasksTab.addEventListener("click", (e) => {
-    if (taskDisplayBox.children.length > 0 ) {
-        let removedElement = taskDisplayBox.firstChild;
-        taskDisplayBox.removeChild(removedElement);
-        // code to display the tasks in the array
-        loadAllTasks(taskDisplayBox, tasksArray);
-        const addTaskButton = document.querySelector(".allTasksTop > button");
+addTaskButton.addEventListener("click", (e) => {
+    form.style.display = "grid";
+});
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const home = document.querySelector(".home");
+    let projectName = document.querySelector("#p_input").value.toUpperCase();
+    if ( projectName == "" ) {
+        projectName = "Not assigned to project.";
+    }
+    let taskStatus = document.querySelector("#s_input");
+    if (taskStatus.checked == true ) {
+        taskStatus = "COMPLETE";
+    } else {
+        taskStatus = "INCOMPLETE";
+    }
+    let taskName = document.querySelector("#n_input").value.toUpperCase();
+    let taskPriority = document.querySelector("#pri_select").value.toUpperCase();
+    let taskDueDate = document.querySelector("#date_input").value.toUpperCase();
+    let taskDescription = document.querySelector("#desc_ta").value.toUpperCase();
+    let taskNotes = document.querySelector("#notes_ta").value.toUpperCase();
+
+    if ( taskName == "" && taskDueDate == "" ) {
+        alert("You must enter a name and due date for this task.")
+    } else {
+        let newTask = buildTask(projectName, taskStatus, taskName, taskPriority, taskDueDate, taskDescription, taskNotes);
+        tasks.push(newTask);
+        form.reset();
+        form.style.display = "none";
+        
+        if ( document.querySelector(".taskList") == null ) {
+            let taskVisual = displayTasks(tasks);
+            home.appendChild(taskVisual);
+        } else {
+            //delete children then add ^^
+            let oldTaskList = document.querySelector(".taskList");
+            home.removeChild(oldTaskList);
+            let taskVisual = displayTasks(tasks);
+            home.appendChild(taskVisual);
+        }
+    }
+});
+
+homeTab.addEventListener("click", (e) => {
+    if ( display.children.length > 0 ) {
+        let removedElement = display.firstChild;
+        display.removeChild(removedElement);
+
+        showHomeHeader();
+        const home = document.querySelector(".home");
+        const addTaskButton = document.querySelector(".addTask");
+        const submit = document.querySelector(".submitTask");
+        const form = document.querySelector("#ntForm");
+    
         addTaskButton.addEventListener("click", (e) => {
-            const formContainer = document.querySelector(".formContainer");
-            formContainer.hidden = false;
-            // code that deletes the previous content 
-            // code that loads the new content
+            form.style.display = "grid";
+        });
+
+        submit.addEventListener("click", (e) => {
+            e.preventDefault();
+        
+            const home = document.querySelector(".home");
+            let projectName = document.querySelector("#p_input").value.toUpperCase();
+            if ( projectName == "" ) {
+                projectName = "Not assigned to project.";
+            }
+            let taskStatus = document.querySelector("#s_input");
+            if (taskStatus.checked == true ) {
+                taskStatus = "COMPLETE";
+            } else {
+                taskStatus = "INCOMPLETE";
+            }
+            let taskName = document.querySelector("#n_input").value.toUpperCase();
+            let taskPriority = document.querySelector("#pri_select").value.toUpperCase();
+            let taskDueDate = document.querySelector("#date_input").value.toUpperCase();
+            let taskDescription = document.querySelector("#desc_ta").value.toUpperCase();
+            let taskNotes = document.querySelector("#notes_ta").value.toUpperCase();
+        
+            if ( taskName == "" && taskDueDate == "" ) {
+                alert("You must enter a name and due date for this task.")
+            } else {
+                let newTask = buildTask(projectName, taskStatus, taskName, taskPriority, taskDueDate, taskDescription, taskNotes);
+                tasks.push(newTask);
+                form.reset();
+                form.style.display = "none";
+                
+                if ( document.querySelector(".taskList") == null ) {
+                    let taskVisual = displayTasks(tasks);
+                    home.appendChild(taskVisual);
+                } else {
+                    //delete children then add ^^
+                    let oldTaskList = document.querySelector(".taskList");
+                    home.removeChild(oldTaskList);
+                    let taskVisual = displayTasks(tasks);
+                    home.appendChild(taskVisual);
+                }
+            }
         });
     }
-})
+});
 
 todayTab.addEventListener("click", (e) => {
-    if (taskDisplayBox.children.length > 0 ) {
-        let removedElement = taskDisplayBox.firstChild;
-        taskDisplayBox.removeChild(removedElement);
-        
-        // code to add inputs for a new task into an object that is added to an array
-        
-        
-        // code to display the tasks in the array
-        
-        
-        loadTodaysTasks(taskDisplayBox);
-    }
-})
+    console.log("today");
+});
 
 weekTab.addEventListener("click", (e) => {
-    if (taskDisplayBox.children.length > 0 ) {
-        let removedElement = taskDisplayBox.firstChild;
-        taskDisplayBox.removeChild(removedElement);
-        
-        // code to add inputs for a new task into an object that is added to an array
-        
-        
-        // code to display the tasks in the array
-        
-        
-        loadWeeksTasks(taskDisplayBox);
-    }
-})
+    console.log("week");
+});
 
 projectsTab.addEventListener("click", (e) => {
-    if (taskDisplayBox.children.length > 0 ) {
-        let removedElement = taskDisplayBox.firstChild;
-        taskDisplayBox.removeChild(removedElement);
-        
-        // code to add inputs for a new task into an object that is added to an array
-        
-        
-        // code to display the tasks in the array
-        
-        
-        loadProjects(taskDisplayBox);
-    }
-})
+    console.log("projects");
+});
