fcf11739eaff2a9193baaa5b64a426253355b402
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-07-02 22:21:48 -0500

    create module that will add project divs to the sidebar based on the number of projects in the projects array

---
+++ src/showProjectSidebar.js
@@ -0,0 +1,34 @@
+const showProjectSidebar = (projectsArray) => {
+
+    const npsd = document.querySelector(".npsd");
+    const allProjectsBox = document.createElement("div");
+    allProjectsBox.classList.add('apBox');
+    npsd.appendChild(allProjectsBox);
+    console.log(npsd);
+
+    for ( let i = 0; i < projectsArray.length; i++ ) {
+        const project = document.createElement("div");
+        project.classList.add("projectContainer");
+        project.setAttribute(`data-projectNumber`, `${i}`);
+    
+        const projectHeader = document.createElement("div");
+        projectHeader.classList.add("projectHeader");
+    
+        const projectTitle = document.createElement("h2");
+        projectTitle.classList.add("projectTitle");
+        projectTitle.innerText = projectsArray[i];
+
+        const deleteProjectButton = document.createElement("button");
+        deleteProjectButton.classList.add("deleteProject");
+        deleteProjectButton.innerText = "X";
+        deleteProjectButton.setAttribute(`data-projectNumber`, `${i}`);
+
+        allProjectsBox.appendChild(project);
+        project.appendChild(projectHeader);
+        projectHeader.appendChild(projectTitle);
+        projectHeader.appendChild(deleteProjectButton);
+    
+    }
+};
+
+export default showProjectSidebar;
