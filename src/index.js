d189fc47e518a915f991378d7385b06efeb950e8
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-07-04 18:08:27 -0500

    add delete and edit functionality to the today and week tab event listeners. Now missing the new form in the headers.

--- src/index.js
+++ src/index.js
@@ -581,7 +581,6 @@ homeTab.addEventListener("click", (e) => {
     };
 });
 
-// we need edit functionality for this tab
 todayTab.addEventListener("click", (e) => {
     if ( display.children.length > 0 ) {
         let removedElement = display.firstChild;
@@ -592,32 +591,82 @@ todayTab.addEventListener("click", (e) => {
         today.appendChild(todaysTasks);
         let todaySelectedList = document.querySelector(".todayTaskList");
 
-        const deleteButtons = document.querySelectorAll(".deleteButton");
-        console.log(deleteButtons);
-        deleteButtons.forEach(e => e.addEventListener("click", (e) => {
-            if (e.target) {
-                let targetButtonIndex = e.target.getAttribute("data-taskNumber");
-                let removedTaskContainer = document.querySelector(`.taskContainer[data-taskNumber="${targetButtonIndex}"]`);
-                todaySelectedList.removeChild(removedTaskContainer)
-                tasks.splice(targetButtonIndex, 1);
-                console.log(tasks);
+        const taskContainers = document.querySelectorAll(".taskContainer");
+        taskContainers.forEach(element => {
+            let elementIndex = element.getAttribute("data-taskNumber");
+            
+            let deletButton = document.querySelector(`.deleteButton[data-taskNumber="${elementIndex}"]`);
+            deletButton.addEventListener("click", (e) => {
+                if (e.target) {
+                    let targetButtonIndex = e.target.getAttribute("data-taskNumber");
+                    let removedTaskContainer = document.querySelector(`.taskContainer[data-taskNumber="${targetButtonIndex}"]`);
+                    todaySelectedList.removeChild(removedTaskContainer);
+                    tasks.splice(targetButtonIndex, 1);
+        
+                    let remainingTasks = document.querySelectorAll(".taskContainer");
+                    let remainingDeleteButtons = document.querySelectorAll(".deleteButton");
+                    let remainingEditButtons = document.querySelectorAll(".editButton");
+        
+                    for ( let i = 0; i < remainingTasks.length; i ++ ) {
+                        remainingTasks[i].setAttribute("data-taskNumber", `${i}`);
+                        remainingDeleteButtons[i].setAttribute("data-taskNumber", `${i}`);
+                        remainingEditButtons[i].setAttribute("data-taskNumber", `${i}`);
+                    };
+                };
+            });
+        
+            let eButton = document.querySelector(`.editButton[data-taskNumber="${elementIndex}"]`);
+            eButton.addEventListener("click", (e) => {
+                if (e.target) {
+                    eButton.disabled = true;
+                    console.log("hey");
+                    editForm.style.display = "grid";
+                    let editButtonIndex = e.target.getAttribute("data-taskNumber");
+                    let editSubmission = document.querySelector(".editSubmitTask");
+                    editSubmission.addEventListener("click", (e) => {
+                        e.preventDefault();
+                        let editProjectName = document.querySelector("#editP_input").value.toUpperCase();
+                        if ( editProjectName == "" ) {
+                            editProjectName = "Not assigned to project.";
+                        }
+                        let editTaskStatus = document.querySelector("#editS_input");
+                        if (editTaskStatus.checked == true ) {
+                            editTaskStatus = "COMPLETE";
+                        } else {
+                            editTaskStatus = "INCOMPLETE";
+                        }
+                        let editTaskName = document.querySelector("#editN_input").value.toUpperCase();
+                        let editTaskPriority = document.querySelector("#editPri_select").value.toUpperCase();
+                        let editTaskDueDate = document.querySelector("#editDate_input").value.toUpperCase();
+                        let editTaskDescription = document.querySelector("#editDesc_ta").value.toUpperCase();
+                        let editTaskNotes = document.querySelector("#editNotes_ta").value.toUpperCase();
+                    
+                        if ( editTaskName == "" || editTaskDueDate == "" ) {
+                            alert("Name and Due Date cannot remain blank.")
+                        } else {
+                            tasks[editButtonIndex].projectName = editProjectName;
+                            tasks[editButtonIndex].taskStatus = editTaskStatus;
+                            tasks[editButtonIndex].taskName = editTaskName;
+                            tasks[editButtonIndex].taskPriority = editTaskPriority;
+                            tasks[editButtonIndex].taskDueDate = editTaskDueDate;
+                            tasks[editButtonIndex].taskDescription = editTaskDescription;
+                            tasks[editButtonIndex].taskNotes = editTaskNotes;
+                            console.log(tasks[editButtonIndex]);
+                            console.log(tasks);
+                            
+                            // code that changes the innertexts of the boxes;
+        
+                            editForm.reset();
+                            editForm.style.display = "none";
+                        };
+                    });     
+                };
+            });
+        });
     
-                let remainingTasks = document.querySelectorAll(".taskContainer");
-                let remainingDeleteButtons = document.querySelectorAll(".deleteButton");
-                let remainingEditButtons = document.querySelectorAll(".editButton");
-    
-                for ( let i = 0; i < remainingTasks.length; i ++ ) {
-                    remainingTasks[i].setAttribute("data-taskNumber", `${i}`);
-                    remainingDeleteButtons[i].setAttribute("data-taskNumber", `${i}`);
-                    remainingEditButtons[i].setAttribute("data-taskNumber", `${i}`);
-                }
-            }
-        }));
-    
-    }
+    };
 });
 
-// we need edit functionality for this tab
 weekTab.addEventListener("click", (e) => {
     if ( display.children.length > 0 ) {
         let removedElement = display.firstChild;
@@ -628,29 +677,79 @@ weekTab.addEventListener("click", (e) => {
         week.appendChild(weeksTasks);
         let weekSelectedList = document.querySelector(".weekTaskList");
 
-        const deleteButtons = document.querySelectorAll(".deleteButton");
-        console.log(deleteButtons);
-        deleteButtons.forEach(e => e.addEventListener("click", (e) => {
-            if (e.target) {
-                let targetButtonIndex = e.target.getAttribute("data-taskNumber");
-                let removedTaskContainer = document.querySelector(`.taskContainer[data-taskNumber="${targetButtonIndex}"]`);
-                weekSelectedList.removeChild(removedTaskContainer)
-                tasks.splice(targetButtonIndex, 1);
-                console.log(tasks);
-    
-                let remainingTasks = document.querySelectorAll(".taskContainer");
-                let remainingDeleteButtons = document.querySelectorAll(".deleteButton");
-                let remainingEditButtons = document.querySelectorAll(".editButton");
-    
-                for ( let i = 0; i < remainingTasks.length; i ++ ) {
-                    remainingTasks[i].setAttribute("data-taskNumber", `${i}`);
-                    remainingDeleteButtons[i].setAttribute("data-taskNumber", `${i}`);
-                    remainingEditButtons[i].setAttribute("data-taskNumber", `${i}`);
-                }
-            }
-        }));
-
-    }
+        const taskContainers = document.querySelectorAll(".taskContainer");
+        taskContainers.forEach(element => {
+            let elementIndex = element.getAttribute("data-taskNumber");
+            
+            let deletButton = document.querySelector(`.deleteButton[data-taskNumber="${elementIndex}"]`);
+            deletButton.addEventListener("click", (e) => {
+                if (e.target) {
+                    let targetButtonIndex = e.target.getAttribute("data-taskNumber");
+                    let removedTaskContainer = document.querySelector(`.taskContainer[data-taskNumber="${targetButtonIndex}"]`);
+                    weekSelectedList.removeChild(removedTaskContainer);
+                    tasks.splice(targetButtonIndex, 1);
+        
+                    let remainingTasks = document.querySelectorAll(".taskContainer");
+                    let remainingDeleteButtons = document.querySelectorAll(".deleteButton");
+                    let remainingEditButtons = document.querySelectorAll(".editButton");
+        
+                    for ( let i = 0; i < remainingTasks.length; i ++ ) {
+                        remainingTasks[i].setAttribute("data-taskNumber", `${i}`);
+                        remainingDeleteButtons[i].setAttribute("data-taskNumber", `${i}`);
+                        remainingEditButtons[i].setAttribute("data-taskNumber", `${i}`);
+                    };
+                };
+            });
+        
+            let eButton = document.querySelector(`.editButton[data-taskNumber="${elementIndex}"]`);
+            eButton.addEventListener("click", (e) => {
+                if (e.target) {
+                    eButton.disabled = true;
+                    console.log("hey");
+                    editForm.style.display = "grid";
+                    let editButtonIndex = e.target.getAttribute("data-taskNumber");
+                    let editSubmission = document.querySelector(".editSubmitTask");
+                    editSubmission.addEventListener("click", (e) => {
+                        e.preventDefault();
+                        let editProjectName = document.querySelector("#editP_input").value.toUpperCase();
+                        if ( editProjectName == "" ) {
+                            editProjectName = "Not assigned to project.";
+                        }
+                        let editTaskStatus = document.querySelector("#editS_input");
+                        if (editTaskStatus.checked == true ) {
+                            editTaskStatus = "COMPLETE";
+                        } else {
+                            editTaskStatus = "INCOMPLETE";
+                        }
+                        let editTaskName = document.querySelector("#editN_input").value.toUpperCase();
+                        let editTaskPriority = document.querySelector("#editPri_select").value.toUpperCase();
+                        let editTaskDueDate = document.querySelector("#editDate_input").value.toUpperCase();
+                        let editTaskDescription = document.querySelector("#editDesc_ta").value.toUpperCase();
+                        let editTaskNotes = document.querySelector("#editNotes_ta").value.toUpperCase();
+                    
+                        if ( editTaskName == "" || editTaskDueDate == "" ) {
+                            alert("Name and Due Date cannot remain blank.")
+                        } else {
+                            tasks[editButtonIndex].projectName = editProjectName;
+                            tasks[editButtonIndex].taskStatus = editTaskStatus;
+                            tasks[editButtonIndex].taskName = editTaskName;
+                            tasks[editButtonIndex].taskPriority = editTaskPriority;
+                            tasks[editButtonIndex].taskDueDate = editTaskDueDate;
+                            tasks[editButtonIndex].taskDescription = editTaskDescription;
+                            tasks[editButtonIndex].taskNotes = editTaskNotes;
+                            console.log(tasks[editButtonIndex]);
+                            console.log(tasks);
+                            
+                            // code that changes the innertexts of the boxes;
+        
+                            editForm.reset();
+                            editForm.style.display = "none";
+                        };
+                    });     
+                };
+            });
+        });
+    };
 });
 
 // there needs to be code that displays already created projects if they're in the projects array.
