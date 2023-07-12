137470cb714271aa1f5c23daa41b6eb0c14c4d6b
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-07-12 18:31:39 -0500

    erase files to start project over

--- src/buildTask.js
+++
@@ -1,5 +0,0 @@
-const buildTask = ( projectName, taskStatus, taskName, taskPriority, taskDueDate, taskDescription, taskNotes ) => {
-    return { projectName, taskStatus, taskName, taskPriority, taskDueDate, taskDescription, taskNotes }
-};
-
-export default buildTask;
